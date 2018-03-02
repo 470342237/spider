188705d73f20bda0db1224528adf0f078500506f
ermao <973657283@qq.com>
2018-03-02 13:11:29 +0800

    修改 spider1.py 格式

--- spider1.py
+++ spider1.py
@@ -19,20 +19,20 @@ def readCommand( argv ):
     parser.add_option("-d", type="int", dest="deep",
 						help="爬虫深度，默认 1 层")
     #parser.add_option("-f", dest="logfile",
-	#					help="log 文件名")
-	#parser.add_option("-l", dest="loglevel",
+    #			help="log 文件名")
+    #parser.add_option("-l", dest="loglevel",
     #                   help="日志文件记录详细程度(1-5)，数字越高越详细", metavar="")
-	#parser.add_option("--testself", action="store_true", dest="testflag", default=False, 
+    #parser.add_option("--testself", action="store_true", dest="testflag", default=False, 
     #                   help="启动自测")
-	#parser.add_option("--thread", type="int", dest="threadnumber", default=10, 
-	#					help="线程池大小，默认10个线程", metavar="THREAD_NUMBER")
-	#parser.add_option("--dbfile", dest="dbfile",
-	#					help="数据库文件名", metavar="DATABASE_FILE")
-	#parser.add_option("--key", dest="key",
-	#					help="设定关键词，仅获取包含关键词的网页", metavar="KEYWORDS")
+    #parser.add_option("--thread", type="int", dest="threadnumber", default=10, 
+    #			help="线程池大小，默认10个线程", metavar="THREAD_NUMBER")
+    #parser.add_option("--dbfile", dest="dbfile",
+    #			help="数据库文件名", metavar="DATABASE_FILE")
+    #parser.add_option("--key", dest="key",
+    #			help="设定关键词，仅获取包含关键词的网页", metavar="KEYWORDS")
     (options, otherargs) = parser.parse_args()
     
-    #
+    # 修改 args 参数
     args = {}
     args['url'] = options.url
     args['deep'] = options.deep
@@ -42,17 +42,9 @@ def readCommand( argv ):
 def webCrawler(url):
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}  
     try:
-        response = urllib2.urlopen(url, timeout=20)
-        if response.info().get('Content-Encoding') == 'gzip':
-            buf = StringIO(response.read())
-            f = gzip.GzipFile(fileobj=buf)
-            html_doc = f.read()
-        else:
-            html_doc = response.read()
-        
-        #request = urllib2.Request(url, headers = headers)
-        #response = urllib2.urlopen(request)
-        #html_doc = response.read()
+        request = urllib2.Request(url, headers = headers)
+        response = urllib2.urlopen(request)
+        html_doc = response.read()
     except UnicodeEncodeError, e:
         print url
     except urllib2.HTTPError, e:
